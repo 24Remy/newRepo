628d4d4e6d770d15c5916579f6a7c21e84ffbf36
DevRemyyy <148792341+24Remy@users.noreply.github.com>
2024-06-12 19:02:02 +0200

    Add files via upload
    This is my project of building a website for my school.

---
+++ js.js
@@ -0,0 +1,19 @@
+//let x =0;
+
+//while(x<=50){
+   // console.log(`${x} remyy`)
+    //x++
+//}//
+
+//let x =0
+//do{
+  //  console.log(`${x} remyy`)
+    //x++;
+//}
+//while(x<=50)
+
+function area(l,w) {
+    console.log(`the result is ${l*w}`)
+}    
+
+area(5,6)
